2fc74490c0ae601f1cdb4203d570b6f446547f41
Jimenez <markjimenez3@gmail.com>
2021-06-07 17:22:21 -0400

    implemente straight left and straight right

--- car-models/dubins_optimal_planner.py
+++ car-models/dubins_optimal_planner.py
@@ -3,7 +3,7 @@ import math
 import matplotlib as mpl
 import numpy as np
 from scipy.optimize import fsolve
-
+from dubins_model import DubinsCar
 
 class DubinsOptimalPlanner:
 
@@ -15,28 +15,50 @@ class DubinsOptimalPlanner:
         self.origin = origin
         self.target = target
 
-    @staticmethod
-    def _left_straight(parameters):
+    def _left_straight(self, parameters):
 
         alpha, distance = parameters
-        car = DubinsOptimalPlanner.dubinsCar
-        r = car.umin
-        x0, y0 = tuple(DubinsOptimalPlanner.origin[:2])
-        xf, yf = tuple(DubinsOptimalPlanner.target[:2])
+        car = self.dubinsCar
+        r = car.velocity / car.umin
+        x0, y0 = tuple(self.origin[:2])
+        xf, yf = tuple(self.target[:2])
         firstEqn = (x0*math.cos(alpha)) - (y0*math.sin(alpha)) + (r*math.sin(alpha)) + distance
         secondEqn = (x0*math.sin(alpha)) + (y0*math.cos(alpha)) + (r*(1-math.cos(alpha)))
 
         return firstEqn, secondEqn
 
+    def _right_straight(self, parameters):
+
+        alpha, distance = parameters
+        car = self.dubinsCar
+        r = car.velocity / car.umin
+        x0, y0 = tuple(self.origin[:2])
+        xf, yf = tuple(self.target[:2])
+        firstEqn = (x0*math.cos(alpha)) - (y0*math.sin(alpha)) + (r*(1-math.cos(alpha))) + distance
+        secondEqn = (x0*math.sin(alpha)) + (y0*math.cos(alpha)) + (r*(math.sin(alpha)))
+
+        return firstEqn, secondEqn
+
     def calculate_dubins_parameters(self, word):
-        if word == "LS":
-            alpha, distance = fsolve(_left_straight, (1.0,1.0))
 
+        if word == 'LS':
+            alpha, distance = fsolve(self._left_straight, (0.0,0.0))
+        if word == 'RS':
+            alpha, distance = fsolve(self._right_straight, (0.0,0.0))
 
+        return alpha, distance
 
 if __name__ == '__main__':
-    dubinsCar = 
-    
 
+    origin = np.array([0.0, 0.0, 0.0])
+    target = np.array([10.0, -10.0, 0.0])
 
+    velocity = 1.0
+    maxSteeringAngle = math.pi / 2.0
+    U = [-1.0 * math.tan(maxSteeringAngle), math.tan(maxSteeringAngle)]
+    dubinsCar = DubinsCar(origin, velocity, U)
 
+    planner = DubinsOptimalPlanner(dubinsCar, origin, target)
+
+    print(planner.calculate_dubins_parameters('LS'))
+    print(planner.calculate_dubins_parameters('RS'))
