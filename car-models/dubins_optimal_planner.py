1e5060ffb783ee8b9411255d7395f1669190be13
Jimenez <markjimenez3@gmail.com>
2021-06-07 15:51:16 -0400

    started optimal planner

--- car-models/dubins_optimal_planner.py
+++ car-models/dubins_optimal_planner.py
@@ -6,6 +6,12 @@ import numpy as np
 
 class DubinsOptimalPlanner:
 
-    def __init__(self):
-        self.gamma
+    def __init__(self, dubinsCar, origin, target):
+        self.dubinsCar = dubinsCar
+        self.straightDistance = 0.0
+        self.turningDistance = 0.0
+        self.origin = origin
+        self.target = target
+
+    def 
 
