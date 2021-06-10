725af5a707bde016d6ba14d118f177c281fa6018
Jimenez <markjimenez3@gmail.com>
2021-06-10 11:55:46 -0400

    got RRT to work

--- RRT.py
+++ RRT.py
@@ -10,15 +10,17 @@ class DubinsCarRRT:
     
     class NodeRRT:
 
-        def __init__(self, position, state=None):
+        def __init__(self, position, path=None):
 
             self.x = position[0] 
             self.y = position[1] 
             self.theta = position[2] 
             self.position = position
-            self.path = {'x':[], 'y':[], 'theta':[]}
             self.parent = None
-            self.state = None
+            self.path = {'x':[], 'y':[], 'theta':[]}
+            if path is not None:
+                self.path = path
+
 
     def __init__(self, dubinsCar, startingPosition, targetList, obstacleList = None, animate = False):
 
@@ -29,26 +31,36 @@ class DubinsCarRRT:
         self.obstacleList = obstacleList
         self.animate = animate
         self.fig = None
-        if(self.animate):
-            self._setup_animation()
+        self.ax = None
+        self.maxIter = 20
+        #if(self.animate):
+            #self._setup_animation()
+
+    def _get_path_from_node_to_point(self, originNode, destinationPoint):
+
+        dubinsState = np.array([originNode.x, originNode.y, originNode.theta])
+        self.car.set_state(dubinsState)
+        planner = DubinsOptimalPlanner(self.car, dubinsState, destinationPoint)
+        path = planner.run()
+
+        return path
 
     def _is_point_reachable(self, originNode, destinationPoint, path=None):
 
+        # checking if target is reachable
         if path is None:
-            dubinsState = np.array([originNode.x, originNode.y, originNode.theta])
-            self.car.set_state(dubinsState)
-            planner = DubinsOptimalPlanner(self.car, dubinsState, destinationPoint)
-            path = planner.run()
-
+            path = self._get_path_from_node_to_point(originNode, destinationPoint)
+        
+        # check every point in path from origin to destination
         for x,y in zip(path['x'], path['y']):
             point = np.array([x,y])
 
+            # invalid path if point collides with obstacle
             for obstacle in self.obstacleList:
                 if abs(np.linalg.norm(point - obstacle[:2])) < obstacle[2]:
-                    print('not a viable path')
                     return False
-        print('found viable path')
-        return True 
+
+        return True
 
     def _calculate_dubins_path_length(self, originNode, destinationPoint):
 
@@ -65,12 +77,53 @@ class DubinsCarRRT:
 
         return path, pathLength
 
-    def _setup_animation():
-        pass
+    def _add_node(self, startNode, shortestPath):
 
-    def _animate():
-        pass
+        carStateAtPoint = np.array([shortestPath['x'][-1], shortestPath['y'][-1], shortestPath['theta'][-1]])
+        nodeToAdd = self.NodeRRT(carStateAtPoint, shortestPath)
+        nodeToAdd.parent = startNode
+        nodeToAdd.path = shortestPath
+        self.nodeList.append(nodeToAdd)
 
+        return nodeToAdd
+
+    def _setup_animation(self):
+
+        plt.ion()
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot()
+        self.ax.set_title('Dubins Car RRT')
+        self.ax.axis('equal')
+        self.ax.set_ylabel('Y-distance(M)')
+        self.ax.set_xlabel('X-distance(M)')
+        self.ax.plot(self.root.x, self.root.y, 'x')
+        plt.show()
+
+    def _animate(self):
+
+        fig = plt.figure()
+
+        plt.plot(self.nodeList[0].x, self.nodeList[0].y, color='red', marker = 'x', markersize=10)
+        for node in self.nodeList[1:]:
+            plt.plot(node.x, node.y, color='black', marker = 'x', markersize=5)
+            i = 0
+            for x, y in zip(node.path['x'], node.path['y']):
+                if i % 1000 == 0:
+                    plt.plot(x, y, color='green', marker = '.', markersize = 2)
+                i += 1
+
+        for obstacle in self.obstacleList:
+            obs = plt.Circle((obstacle[0], obstacle[1]), obstacle[2], color='red', fill=False)
+            plt.gca().add_patch(obs)
+
+        for target in self.targetList:
+            tar = plt.Circle((target[0], target[1]), target[2], color='blue', fill=False)
+            plt.gca().add_patch(tar)
+
+        plt.axis("equal")
+        plt.show()
+
+    # RRT ALGORITHM
     def simulate(self):
         
         targetIdx = 0
@@ -80,10 +133,14 @@ class DubinsCarRRT:
         # check for valid path from root to target
         isTargetReachable = self._is_point_reachable(self.root, target)
 
-        while not isTargetReachable:
+        iteration = 0
+
+        while False and not isTargetReachable and iteration < self.maxIter:
+
+            iteration += 1
 
             # sample random point
-            randomPoint = np.random.uniform(low = -10.0, high = 10.0, size = (2,)) 
+            randomPoint = np.random.uniform(low = -5.0, high = 5.0, size = (2,)) 
 
             # setup to begin search 
             shortestPath = None
@@ -93,8 +150,9 @@ class DubinsCarRRT:
             # search tree for nearest neighbor to new point
             for node in self.nodeList:
                 
-                # nodes that cannot turn fast enough to reach point
-                if abs(np.linalg.norm(node.position[2] - randomPoint)) < (2.0 * self.car.minTurningRadius):
+                # ignore nodes that are too close to point 
+                if abs(np.linalg.norm(node.position[:2] - randomPoint)) < (2.0 * self.car.minTurningRadius):
+
                     continue
 
                 # get dubins optimal path and length
@@ -102,16 +160,28 @@ class DubinsCarRRT:
 
                 # store shortest path
                 if shortestPathLength is None or pathLength < shortestPathLength:
+
                     shortestPathLength = pathLength
                     shortestPath = path
                     startNode = node
 
             # check for viable path from parent node to new point
-            if self._is_point_reachable(startNode, randomPoint, shortestPath):
-                carStateAtPoint = np.array([shortestPath['x'][-1], shortestPath['y'][-1], shortestPath['theta'][-1]])
-                nodeToAdd = self.NodeRRT(carStateAtPoint)
+            if startNode is not None and self._is_point_reachable(startNode, randomPoint, shortestPath):
 
-def test_dubins_car_RRT():
+                self._add_node(startNode, shortestPath)
+                isTargetReachable = self._is_point_reachable(self.nodeList[-1], target)
+
+        
+        if False and iteration < self.maxIter:
+            # finally, connect last node to target and add target to nodelist
+            finalPathToTarget = self._get_path_from_node_to_point(self.nodeList[-1], target)
+            self._add_node(self.nodeList[-1], finalPathToTarget)
+
+        self._animate()
+
+
+
+def test_dubins_car_RRT(animate=False):
 
     # set car original position
     startPosition = np.array([0.0, 0.0, 0.0])
@@ -127,16 +197,20 @@ def test_dubins_car_RRT():
     targetList = [np.array(target) for target in targetList]
 
     # set obstacles (x, y, radius)
-    obstacleList = [[2.5, 2.5, 1.0]]
+    obstacleList = [[3.0, 5.0, 0.5], [5.0, 3.0, 1.0], [2.0, 3.0, 0.8], [3.0, 3.0, 0.5], [6.0, 2.0, 2.0]]
     obstacleList = [np.array(obstacle) for obstacle in obstacleList]
 
-    rrtSimulator = DubinsCarRRT(dubinsCar, startPosition, targetList, obstacleList)
+    rrtSimulator = DubinsCarRRT(dubinsCar, startPosition, targetList, obstacleList, animate=animate)
 
     path = rrtSimulator.simulate()
 
 if __name__ == '__main__':
 
-    test_dubins_car_RRT()
+    animate = False
+    if len(sys.argv) > 1:
+        animate = True
+
+    test_dubins_car_RRT(animate)
         
     
 
