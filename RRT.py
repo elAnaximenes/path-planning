9a6e1bd2d545b10091e9f4bbd40a4f34399e07dc
Jimenez <markjimenez3@gmail.com>
2021-06-11 12:09:31 -0400

    got RRT to animate very nicely

--- RRT.py
+++ RRT.py
@@ -35,8 +35,9 @@ class DubinsCarRRT:
         self.fig = None
         self.ax = None
         self.maxIter = 100
-        #if(self.animate):
-            #self._setup_animation()
+        self.pathFromStartToTarget = {'nodes': [], 'path': {'x': [], 'y': [], 'theta': []}} 
+        if(self.animate):
+            self._setup_animation()
 
     def _get_path_from_node_to_point(self, originNode, destinationPoint):
 
@@ -49,18 +50,26 @@ class DubinsCarRRT:
 
     def _is_point_reachable(self, originNode, destinationPoint, path=None):
 
+        if originNode == None:
+            return False
+
         # checking if target is reachable
         if path is None:
             path = self._get_path_from_node_to_point(originNode, destinationPoint)
         
-        # check every point in path from origin to destination
+        i = 0
+        # check for obstacle collisions
         for x,y in zip(path['x'], path['y']):
             point = np.array([x,y])
 
-            # invalid path if point collides with obstacle
-            for obstacle in self.obstacleList:
-                if abs(np.linalg.norm(point - obstacle[:2])) < obstacle[2]:
-                    return False
+            if i % 100 == 0:
+
+                # invalid path if point collides with obstacle
+                for obstacle in self.obstacleList:
+                    if abs(np.linalg.norm(point - obstacle[:2])) < obstacle[2]:
+                        return False
+
+            i += 1
 
         return True
 
@@ -91,46 +100,103 @@ class DubinsCarRRT:
     
     def _setup_animation(self):
 
-        plt.ion()
         self.fig = plt.figure()
         self.ax = self.fig.gca()
         self.ax.set_title('Dubins Car RRT')
-        self.ax.axis('equal')
+        plt.xlim(-12.0, 12.0)
+        plt.ylim(-12.0, 12.0)
+        self.ax.set_aspect('equal')
         self.ax.set_ylabel('Y-distance(M)')
         self.ax.set_xlabel('X-distance(M)')
         self.ax.plot(self.root.x, self.root.y, 'x')
 
         for obstacle in self.obstacleList:
+
             obs = plt.Circle((obstacle[0], obstacle[1]), obstacle[2], color='red', fill=False)
             self.ax.add_patch(obs)
             self.fig.canvas.draw()
+            plt.pause(0.0001)
 
         for target in self.targetList:
+
             tar = plt.Circle((target[0], target[1]), target[2], color='blue', fill=False)
             self.ax.add_patch(tar)
             self.fig.canvas.draw()
+            plt.pause(0.0001)
     
-    def _animate(self):
-        pass
+    def _update_animation(self, point, path, event):
+ 
+        if path is None:
+            return 
+
+        pathToPlot = {'x': [], 'y':[]}
+
+        i = 0
+        for x, y in zip(path['x'], path['y']):
+            sparsenessOfPath = 1000
+
+            if i % sparsenessOfPath == 0:
+                pathToPlot['x'].append(x)
+                pathToPlot['y'].append(y)
+            i += 1       
+
+        if event == 'candidate':
+            plottedPoint, = self.ax.plot(point[0], point[1], color='black', marker = 'x', markersize=5)
+            plottedPath, = self.ax.plot(pathToPlot['x'], pathToPlot['y'], color = 'orange',  linestyle='dotted', markersize = 2)
+            plt.pause(0.1)
+            
+            plottedPoint.remove()
+            plottedPath.remove()
+
+        else:
+            if event == 'valid path':
+                colorSelection = 'green'
+
+            elif event == 'invalid path':
+                colorSelection = 'red'
+
+            elif event == 'reached target':
+                colorSelection = 'blue'
+
+            plottedPoint, = self.ax.plot(point[0], point[1], color='black', marker = 'x', markersize=5)
+            plottedPath, = self.ax.plot(pathToPlot['x'], pathToPlot['y'], color=colorSelection, marker='.', markersize=2)
+            plt.pause(0.1)
+
+            if event == 'invalid path':
+                plottedPath.remove()
+                plottedPoint.remove()
         
-    def _animate_static(self):
+    def _draw_static(self):
 
         fig = plt.figure()
-
+        # root
         plt.plot(self.nodeList[0].x, self.nodeList[0].y, color='red', marker = 'x', markersize=10)
+
         for node in self.nodeList[1:]:
+
             plt.plot(node.x, node.y, color='black', marker = 'x', markersize=5)
+
             i = 0
             for x, y in zip(node.path['x'], node.path['y']):
+
                 if i % 1000 == 0:
                     plt.plot(x, y, color='green', marker = '.', markersize = 2)
                 i += 1
 
+        i = 0
+        for x, y in zip(self.pathFromStartToTarget['path']['x'], self.pathFromStartToTarget['path']['y']):
+
+            if i % 1000 == 0:
+                plt.plot(x, y, color='blue', marker = '.', markersize = 2)
+            i += 1
+
         for obstacle in self.obstacleList:
+
             obs = plt.Circle((obstacle[0], obstacle[1]), obstacle[2], color='red', fill=False)
             plt.gca().add_patch(obs)
 
         for target in self.targetList:
+
             tar = plt.Circle((target[0], target[1]), target[2], color='blue', fill=False)
             plt.gca().add_patch(tar)
 
@@ -140,7 +206,7 @@ class DubinsCarRRT:
     # RRT ALGORITHM
     def simulate(self):
         
-        targetIdx = 0
+        targetIdx = random.randint(0, len(self.targetList) - 1)
         # select target from list
         target = self.targetList[targetIdx]
 
@@ -163,10 +229,9 @@ class DubinsCarRRT:
             
             # search tree for nearest neighbor to new point
             for node in self.nodeList:
-                
+
                 # ignore nodes that are too close to point 
                 if abs(np.linalg.norm(node.position[:2] - randomPoint)) < (2.0 * self.car.minTurningRadius):
-
                     continue
 
                 # get dubins optimal path and length
@@ -174,47 +239,72 @@ class DubinsCarRRT:
 
                 # store shortest path
                 if shortestPathLength is None or pathLength < shortestPathLength:
-
                     shortestPathLength = pathLength
                     shortestPath = path
                     startNode = node
 
-            # check for viable path from parent node to new point
-            if startNode is not None and self._is_point_reachable(startNode, randomPoint, shortestPath):
+                    if self.animate:
+                        self._update_animation(point=randomPoint, path=shortestPath, event='candidate')
 
+            isPointReachable = self._is_point_reachable(startNode, randomPoint, shortestPath)
+            # check for viable path from parent node to new point
+            if isPointReachable:
                 self._add_node(startNode, shortestPath)
                 isTargetReachable = self._is_point_reachable(self.nodeList[-1], target)
 
+                if self.animate:
+                    self._update_animation(point=randomPoint, path=shortestPath, event='valid path')
+
+            elif self.animate and not isPointReachable:
+                self._update_animation(point=randomPoint, path=shortestPath, event='invalid path')
         
         if iteration < self.maxIter:
             # finally, connect last node to target and add target to nodelist
             finalPathToTarget = self._get_path_from_node_to_point(self.nodeList[-1], target)
-            self._add_node(self.nodeList[-1], finalPathToTarget)
+            targetNode = self._add_node(self.nodeList[-1], finalPathToTarget)
 
-        self._animate_static()
+            node = targetNode
 
-def load_scene():
+            while node is not None:
+
+                self.pathFromStartToTarget['nodes'].append(node)
+                self.pathFromStartToTarget['path']['x'] = node.path['x'] + self.pathFromStartToTarget['path']['x']
+                self.pathFromStartToTarget['path']['y'] = node.path['y'] + self.pathFromStartToTarget['path']['y']
+                self.pathFromStartToTarget['path']['theta'] = node.path['theta'] + self.pathFromStartToTarget['path']['theta']
+                node = node.parent
+
+            if self.animate:
+                self._update_animation(target, path=self.pathFromStartToTarget['path'], event='reached target') 
+
+        if self.animate:
+            plt.show()
+        else:
+            self._draw_static()
+
+def load_scene(sceneSelection):
 
     scene = None
 
-    with open('./data/scene.json') as f:
+    with open('./scenes/{}.json'.format(sceneSelection)) as f:
         scene = json.load(f)
 
     targets = scene['targets']
     targetList = []
     for target in targets:
+
         t = [target['x'], target['y'], target['radius']]
         targetList.append(t)
 
     obstacles = scene['obstacles']
     obstacleList = []
     for obstacle in obstacles:
+
         o = [obstacle['x'], obstacle['y'], obstacle['radius']]
         obstacleList.append(o)
     
     return targetList, obstacleList
 
-def test_dubins_car_RRT(animate=False):
+def test_dubins_car_RRT(sceneSelection, animate=False):
 
     # set car original position
     startPosition = np.array([0.0, 0.0, 0.0])
@@ -223,7 +313,8 @@ def test_dubins_car_RRT(animate=False):
     velocity = 1.0
     maxSteeringAngle = (math.pi / 4.0) 
     U = [-1.0 * math.tan(maxSteeringAngle), math.tan(maxSteeringAngle)]
-    dubinsCar = DubinsCar(startPosition, velocity, U)
+    timeStep = 0.0001
+    dubinsCar = DubinsCar(startPosition, velocity, U, dt=timeStep)
 
     # get scene information
     targetList, obstacleList = load_scene() 
@@ -234,11 +325,18 @@ def test_dubins_car_RRT(animate=False):
 
 if __name__ == '__main__':
 
+    print('usage: {} scene_name [animate]'.format(sys.argv[0]))
+    
+    sceneSelection = 'cluttered_room'
+
+    if len(sys.argv == 1):
+        sceneSelection = sys.argv[1]
+
     animate = False
-    if len(sys.argv) > 1:
+    if len(sys.argv) > 2:
         animate = True
 
-    test_dubins_car_RRT(animate)
+    test_dubins_car_RRT(sceneSelection, animate)
         
     
 
